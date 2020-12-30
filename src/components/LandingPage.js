6b09a80eaa128eaae4786ee70e61de8d190c1901
Sodienye Nkwonta <sodienye.nkwonta@ucalgary.ca>
2020-12-30 01:28:40 -0700

    scrambling hover text added and everything aligned

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,27 +6,38 @@ class LandingPage extends Component {
         return (
             <div style={{width: '100', margin: 'auto'}}>
                 <Grid className="landing-grid">
-                    <Cell col={12}></Cell>
-                    <img
-                        src="https://cdn4.iconfinder.com/data/icons/occupation-and-people-avatar-vol-3-2/128/person_avatar_student_young_people_man_skinhead-512.png"
-                        alt="avatar"
-                        className="avatar-img"
-                    />
-
+                    <Cell col={12}>
+                        <img
+                            src="https://cdn4.iconfinder.com/data/icons/occupation-and-people-avatar-vol-3-2/128/person_avatar_student_young_people_man_skinhead-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                    </Cell>
                     <div className="typewriter">
                         <h1>Aspiring Web Developer</h1>
                     </div>
 
-                    <hr/>
-                    <p>HTML/CSS | Python | JavaScript</p>
+                    {/* <div class="loading">
+                        <span>Loading</span>
+                    </div> */}
 
 
                 </Grid>
-                <div class="loading">
-                        <span>Loading</span>
+
+                <div className="scrambler">
+                    <h4>
+                        <span>HTML/CSS | Python | JavaScript</span>
+                        <span>+T\L/[S5 | 9yt#0n | J\v/S[ri6t</span>
+                        <span>H=M|/CS2 | P4+ho^ | 7a^a5c?1p+</span>
+                        <span>H%;-/CS* | 8yt@o! | J'v$,c|i:t</span>
+                        <span>'T!L/~S@ | P#t%o( | J_va$c^ip-</span>
+                    </h4>
                 </div>
+
+
                 
             </div>
+            
         )
     }
 }
