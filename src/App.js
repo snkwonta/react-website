f4ece30a6a1c3474131835a4f91449bbab7427f7
Sodienye Nkwonta <sodienye.nkwonta@ucalgary.ca>
2020-12-29 18:07:44 -0700

    added typewriter text and dummy profile photo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {Header, Navigation, Drawer, Content, Layout} from 'react-mdl'
 import Main from './components/main';
 import { Link } from 'react-router-dom'; 
 
-function App() {
-  return (
-    <div className="demo-big-content">
-      <Layout>
-            <Header className="header-color" title="Title" scroll>
-                <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contactme">Contact Me</Link>
-                </Navigation>
-            </Header>
-            <Drawer title="Testing">
-                <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/about">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contactme">Contact Me</Link>
-                </Navigation>
-            </Drawer>
-            <Content>
-              <div className="page-content"/>
-              <Main/>
-            </Content>
-        </Layout>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+              <Header className="header-color" title="Title" scroll>
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/about">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contactme">Contact Me</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Testing">
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/about">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contactme">Contact Me</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                <div className="page-content"/>
+                <Main/>
+              </Content>
+          </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { Cell, Grid } from 'react-mdl';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component {
     render(){
         return (
-            <div style={{width: 100, margin: 'auto'}}>
+            <div style={{width: '100', margin: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12}></Cell>
                     <img
-                        src=""
+                        src="https://cdn4.iconfinder.com/data/icons/occupation-and-people-avatar-vol-3-2/128/person_avatar_student_young_people_man_skinhead-512.png"
                         alt="avatar"
                         className="avatar-img"
                     />
+
+                    <div className="typewriter">
+                        <h1>Aspiring Web Developer</h1>
+                    </div>
+
+                    <hr/>
+                    <p>HTML/CSS | Python | JavaScript</p>
+
+
                 </Grid>
+                <div class="loading">
+                        <span>Loading</span>
+                </div>
                 
             </div>
         )
