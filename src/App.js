f0513b8714b7c68e6bd1120744ae7288f7d0d6fa
Sodienye Nkwonta <sodienye.nkwonta@ucalgary.ca>
2021-01-21 15:11:47 -0700

    working on projects page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 class App extends Component{
   render() {
     return (
+      
       <div className="demo-big-content">
         <Layout>
               <Header className="header-color" title="Title" scroll>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import {Tab, Tabs} from 'react-mdl';
+import {Cell, Grid, Tab, Tabs, Card, CardActions, Button, CardMenu, IconButton, CardTitle} from 'react-mdl';
 
 class Projects extends Component {
     constructor(props){
         super(props);
         this.state = {activeTab: 0}
     }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <Card shadow={5} style={{minWidth: '250', margin:'auto'}}>
+                    <CardTitle style={{color: '#ffff'}}></CardTitle>
+
+                </Card>
+            )
+        }else if(this.state.activeTab === 1){
+            return(
+                <div><h1>This is Java</h1></div>
+            )
+
+        }else if(this.state.activeTab === 2){
+            return(
+                <div><h1>This is HTML</h1></div>
+            )
+
+        }
+
+    }
+
     render(){
         return (
             <div className="category-tabs">
@@ -15,6 +38,19 @@ class Projects extends Component {
                     <Tab>HTML</Tab>
 
                 </Tabs>
+
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">
+                                {this.toggleCategories()}
+                            </div>
+
+                        </Cell>
+                    </Grid>
+                    
+
+                </section>
             </div>
         )
     }
