dc94ea40c7b506ed0c53ff3827562b9d8708d6ab
Sodienye Nkwonta <sodienye.nkwonta@ucalgary.ca>
2021-01-22 14:46:05 -0700

    working on hover effect on cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,77 +1,136 @@
 import React, { Component } from 'react';
-import {Cell, Grid, Tab, Tabs, Card, CardActions, Button, CardMenu, IconButton, CardTitle, CardText} from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+
 
 class Projects extends Component {
-    constructor(props){
-        super(props);
-        this.state = {activeTab: 0}
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.inovex.de/blog/wp-content/uploads/2022/01/one-year-of-react-native.png) center / cover'}} >This Webiste!</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored href="https://github.com/snkwonta/react-website" target="_blank">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
+
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hotforsecurity.bitdefender.com/wp-content/uploads/2013/02/java-logo.png) center / cover'}} >School Project</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hotforsecurity.bitdefender.com/wp-content/uploads/2013/02/java-logo.png) center / cover'}} >React Project #2</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://hotforsecurity.bitdefender.com/wp-content/uploads/2013/02/java-logo.png) center / cover'}} >React Project #3</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://store-images.s-microsoft.com/image/apps.40439.13902272735533786.62dcd87f-a7f1-4a5f-a2b7-ff69f15a9bcc.fee5bed0-c445-44c4-b6cb-09ae31b4c69b?mode=scale&q=90&h=1080&w=1920) center / cover'}} >Reversi Game</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored href="https://github.com/snkwonta/Reversi" target="_blank">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.princetonreview.com/cms-content/CanYouUseCalculatoronGMAT.jpg) center / cover'}} >HTML Calculator</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored href="https://github.com/snkwonta/snkwonta.github.io" target="_blank">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      )
     }
 
-    toggleCategories(){
-        if(this.state.activeTab === 0){
-            return(
-                <Card shadow={5} style={{minWidth: '250', margin:'auto'}}>
-                    <CardTitle style={{color: '#ffff', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover' }}></CardTitle>
-                    <CardText>This Portfolio Website</CardText>
-                    <CardActions border>
-                        <Button colored href="https://github.com/snkwonta/react-website" target='_blank'>GitHub</Button>
-                    </CardActions>
+  }
+  render() {
+    return(
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>React</Tab>
+          <Tab>Java</Tab>
+          <Tab>HTML</Tab>
+        </Tabs>
 
-                </Card>
-            )
-        }else if(this.state.activeTab === 1){
-            return(
-                <Card shadow={5} style={{minWidth: '250', margin:'auto'}}>
-                <CardTitle style={{color: '#ffff', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover' }}></CardTitle>
-                <CardText>School Project</CardText>
-                <CardActions border>
-                    <Button colored href="https://github.com/snkwonta/PalindromeArray" target='_blank'>GitHub</Button>
-                </CardActions>
 
-            </Card>
-            )
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
 
-        }else if(this.state.activeTab === 2){
-            return(
-                <Card shadow={5} style={{minWidth: '250', margin:'auto'}}>
-                <CardTitle style={{color: '#ffff', height: '176px', background:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover' }}></CardTitle>
-                <CardText>Reversi Game</CardText>
-                <CardActions border>
-                    <Button colored href="https://github.com/snkwonta/Reversi" target='_blank'>GitHub</Button>
-                </CardActions>
 
-            </Card>
-            )
-
-        }
-
-    }
-
-    render(){
-        return (
-            <div className="category-tabs">
-                <Tabs activeTab = {this.state.activeTab} onChange={tabID => this.setState({activeTab:tabID})} ripple>
-                    <Tab>React</Tab>
-                    <Tab>Java</Tab>
-                    <Tab>HTML</Tab>
-
-                </Tabs>
-
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
-                        <Cell col={12}>
-                            <div className="content">
-                                {this.toggleCategories()}
-                            </div>
-
-                        </Cell>
-                    </Grid>
-                    
-
-                </section>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default Projects;
